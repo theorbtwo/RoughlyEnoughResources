ae4ef7535c50a3dd4002e82cab70a8e97601075a
James Mastros <theorbtwo@gmail.com>
2019-06-14 16:55:28 +0100

    stuff

--- src/main/java/uk/me/desert_island/rer/REIPlugins.java
+++ src/main/java/uk/me/desert_island/rer/REIPlugins.java
@@ -1,7 +1,10 @@
 package uk.me.desert_island.rer;
 
+import me.shedaniel.rei.api.REIPluginEntry;
+import net.minecraft.util.Identifier;
+
 public class REIPlugins implements REIPluginEntry {
-    public static final Identifier PLUGIN = new Identifier("roughlyenoughresources", "rer_plugin");
+    public static final Identifier PLUGIN_ID = new Identifier("roughlyenoughresources", "rer_plugin");
     
     @Override public Identifier getPluginIdentifier() {
 	return PLUGIN_ID;
---
+++ src/main/java/uk/me/desert_island/rer/rei_stuff/WorldGenRecipe.java
@@ -0,0 +1,49 @@
+package uk.me.desert_island.rer.rei_stuff;
+
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+public class WorldGenRecipe implements Recipe<Inventory> {
+    public ItemStack output;
+
+    @Override
+    public ItemStack craft(Inventory arg0) {
+        return null;
+    }
+
+    @Override
+    public boolean fits(int arg0, int arg1) {
+        return false;
+    }
+
+    @Override
+    public Identifier getId() {
+        return null;
+    }
+
+    @Override
+    public ItemStack getOutput() {
+        return output;
+    }
+
+    @Override
+    public RecipeSerializer getSerializer() {
+        return null;
+    }
+
+    @Override
+    public RecipeType getType() {
+        return null;
+    }
+
+    @Override
+    public boolean matches(Inventory arg0, World arg1) {
+        return false;
+    }
+
+}

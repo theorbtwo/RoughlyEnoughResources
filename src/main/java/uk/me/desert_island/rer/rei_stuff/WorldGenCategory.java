9752224d39f611a96f2a5c2fc5d20ee6093e2192
Nico Mexis <nico.mexis@kabelmail.de>
2021-12-16 10:09:02 +0100

    Change to up and down arrows

--- src/main/java/uk/me/desert_island/rer/rei_stuff/WorldGenCategory.java
+++ src/main/java/uk/me/desert_island/rer/rei_stuff/WorldGenCategory.java
@@ -181,11 +181,11 @@ public class WorldGenCategory implements DisplayCategory<WorldGenDisplay> {
         widgets.add(Widgets.createSlot(new Point(bounds.getMaxX() - (16), bounds.getMinY() + 3)).entries(display.getOutputEntries().get(0)));
         widgets.add(Widgets.createLabel(new Point(bounds.x + 65, bounds.getMaxY() - 10), new LiteralText(Registry.BLOCK.getId(block).toString())).noShadow().color(-12566464, -4473925));
 
-        Button scrollLeft = Widgets.createButton(new Rectangle(bounds.x + 1, bounds.getMaxY(), 16, 16), new LiteralText("-"));
+        Button scrollLeft = Widgets.createButton(new Rectangle(bounds.x + 1, bounds.getMaxY(), 16, 16), new LiteralText("\u2193"));
         scrollLeft.setOnClick(button -> scroll(-50));
         widgets.add(scrollLeft);
 
-        Button scrollRight = Widgets.createButton(new Rectangle(bounds.x + 114, bounds.getMaxY(), 16, 16), new LiteralText("+"));
+        Button scrollRight = Widgets.createButton(new Rectangle(bounds.x + 114, bounds.getMaxY(), 16, 16), new LiteralText("\u2191"));
         scrollRight.setOnClick(button -> scroll(50));
         widgets.add(scrollRight);
 

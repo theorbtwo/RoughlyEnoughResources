6c403322552b9f45678fc7edee3267fed142cca6
shedaniel <daniel@shedaniel.me>
2022-04-06 14:56:54 +0800

    Fix OnlyIn
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- forge/src/main/java/uk/me/desert_island/rer/forge/RoughlyEnoughResourcesForge.java
+++ forge/src/main/java/uk/me/desert_island/rer/forge/RoughlyEnoughResourcesForge.java
@@ -2,6 +2,8 @@ package uk.me.desert_island.rer.forge;
 
 import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod;
 import uk.me.desert_island.rer.RoughlyEnoughResources;
 import uk.me.desert_island.rer.RoughlyEnoughResourcesClient;
@@ -13,6 +15,7 @@ public class RoughlyEnoughResourcesForge {
         EnvExecutor.runInEnv(Env.CLIENT, () -> Client::run);
     }
 
+    @OnlyIn(Dist.CLIENT)
     private static class Client {
         public static void run() {
             RoughlyEnoughResourcesClient.onInitializeClient();
